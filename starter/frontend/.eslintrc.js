d86eba6875a65f289bca20e5ba5f2183ed29e71c
Harshwardhan <harshwardhan321@gmail.com>
2026-03-10 11:22:36 +0000

    format eslint config with prettier

--- starter/frontend/.eslintrc.js
+++ starter/frontend/.eslintrc.js
@@ -3,15 +3,14 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   extends: [
-  "eslint:recommended",
-  "plugin:@typescript-eslint/recommended",
-  "plugin:prettier/recommended",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
   env: {
-  browser: true,
-  es2021: true,
-  node: true,
+    browser: true,
+    es2021: true,
+    node: true,
   },
   rules: {},
-  };
-  
+};
