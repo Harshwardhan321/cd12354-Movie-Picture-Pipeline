9cdffbe24a133e2ff6d8fa548a148af2b84a1f4c
Harshwardhan <harshwardhan321@gmail.com>
2026-03-10 11:18:04 +0000

    fix eslint configuration formatting

--- starter/frontend/.eslintrc.js
+++ starter/frontend/.eslintrc.js
@@ -1,37 +1,17 @@
 module.exports = {
-  ignorePatterns: ['build/', 'dist/', 'node_modules/'],
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  plugins: ['react', 'prettier'],
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+  "eslint:recommended",
+  "plugin:@typescript-eslint/recommended",
+  "plugin:prettier/recommended",
+  ],
   env: {
-    browser: true,
-    es6: true,
-    node: true,
-    jest: true,
+  browser: true,
+  es2021: true,
+  node: true,
   },
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        jsxSingleQuote: false,
-        printWidth: 120,
-        parser: 'babel-ts',
-      },
-    ],
-    // Customize your rules here
-    'react/prop-types': process.env.FAIL_LINT ? 2 : 0,
-    'react/jsx-uses-vars': 'warn',
-  },
-};
+  rules: {},
+  };
+  
