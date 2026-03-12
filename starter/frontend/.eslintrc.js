208bfee6da645622907bdd8ae8748186b5486f0b
Harshwardhan <harshwardhan321@gmail.com>
2026-03-12 07:01:07 +0000

    reset eslint config

--- starter/frontend/.eslintrc.js
+++ starter/frontend/.eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  rules: {},
-};
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended"
+    ],
+    env: {
+      browser: true,
+      es2021: true,
+      node: true
+    },
+    rules: {}
+  };
